64af1557abe5ec419743b45b8443af168ebc8359
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-12-02 20:45:59 -0500

    feat: add liveness check

---
+++ src/main/java/com/soulware/therapysystem/profiles/profiles/infrastructure/liveness/LivenessCheck.java
@@ -0,0 +1,35 @@
+package com.soulware.therapysystem.profiles.profiles.infrastructure.liveness;
+
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+    @Resource(lookup = "java:jboss/datasources/{datasource del wildfly perteneciente a este}")
+            DataSource dataSource;
+
+    @Override
+    public HealthCheckResponse call() {
+        boolean dbOk = checkDb();
+        return dbOk ?
+                HealthCheckResponse.up("db") :
+                HealthCheckResponse.down("db");
+    }
+
+    private boolean checkDb() {
+        try (Connection conn = dataSource.getConnection()) {
+            return conn.isValid(2);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
